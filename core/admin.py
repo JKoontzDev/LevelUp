12aa3f377d502b46b04e4ffe3c965496eedd9f5b
Sadface <SadfaceDigitalSolutions@gmail.com>
2025-08-22 11:17:52 -0400

    fix blog admin

--- core/admin.py
+++ core/admin.py
@@ -326,14 +326,7 @@ class BlogPostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('created_at', 'author')
     ordering = ('-created_at',)
-    fieldsets = (
-        ('details', {'fields': ('title', 'author', 'created_at', 'updated_at')}),)
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('title', 'author', 'created_at',),
-        }),
-    )
+
 
 
 
